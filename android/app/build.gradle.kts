67992d1e5836b804728b81e61baa0e86b04a7e58
NavidFarahmandian <zroidir@gmail.com>
2025-05-11 11:58:20 +0330

    Added Compile Settings.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,20 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// load your keystore properties
+val keystorePropertiesFile = file("key.properties")
+val keystoreProperties = Properties().apply {
+    if (keystorePropertiesFile.exists()) {
+        // now that we’ve imported FileInputStream, load() is known
+        load(FileInputStream(keystorePropertiesFile))
+    }
 }
 
 android {
@@ -27,9 +39,31 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+                    ?: error("Missing keyAlias in keystore.properties")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+                    ?: error("Missing keyPassword in keystore.properties")
+            // now Kotlin can infer the type of 'it' as String, so no lambda-inference error
+            storeFile = keystoreProperties.getProperty("storeFile")?.let { it: String ->
+                file(it)
+            } ?: error("Missing storeFile in keystore.properties")
+            storePassword = keystoreProperties.getProperty("storePassword")
+                    ?: error("Missing storePassword in keystore.properties")
+        }
+    }
+
     buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            multiDexEnabled = true
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    file("proguard-rules.pro")
+            )
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,21 @@
+#-keep class io.flutter.app.** { *; }
+-keep class io.flutter.plugin.** { *; }
+-keep class io.flutter.util.** { *; }
+-keep class io.flutter.view.** { *; }
+#-keep class io.flutter.** { *; }
+-keep class io.flutter.plugins.** { *; }
+# SPDX-FileCopyrightText: 2016, microG Project Team
+# SPDX-License-Identifier: CC0-1.0
+
+# Keep AutoSafeParcelables
+-keep public class * extends org.microg.safeparcel.AutoSafeParcelable {
+    @org.microg.safeparcel.SafeParcelable.Field *;
+    @org.microg.safeparcel.SafeParceled *;
+}
+
+# Keep asInterface method cause it's accessed from SafeParcel
+-keepattributes InnerClasses
+-keepclassmembers interface * extends android.os.IInterface {
+    public static class *;
+}
+-keep public class * extends android.os.Binder { public static *; }
